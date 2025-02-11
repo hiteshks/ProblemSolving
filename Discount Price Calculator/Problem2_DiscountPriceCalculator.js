37c180591e68697f25ab7e55edb9eaa3f724a2de
Hitesh Kumar Siwan <hiteshk888@gmail.com>
2025-02-12 01:11:38 +0530

    Solved Discount Price Calculator

--- Discount Price Calculator/Problem2_DiscountPriceCalculator.js
+++ Discount Price Calculator/Problem2_DiscountPriceCalculator.js
@@ -1,6 +1,23 @@
-let a = 30;
-let b = 100;
 
-let sum = a+b;
-console.log(sum);
-console.log("Hello world");
+function discountPriceCalculator(price){
+    
+    if(price>20){
+
+        let discountedPrice = price * (10/100);
+        
+        let finalPrice = price - discountedPrice;
+
+        return finalPrice;
+
+    }
+    else if(price<=20){
+
+        return price
+
+    }
+
+}
+
+console.log(discountPriceCalculator(25));
+console.log(discountPriceCalculator(20));
+console.log(discountPriceCalculator(15));
