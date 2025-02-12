4035d904e2c429cab5a05e2c7cb5cd76999c52cb
Hitesh Kumar Siwan <hiteshk888@gmail.com>
2025-02-12 23:03:12 +0530

    Added comments

--- DiscountPriceCalculator/Problem2_DiscountPriceCalculator.js
+++ DiscountPriceCalculator/Problem2_DiscountPriceCalculator.js
@@ -1,23 +1,25 @@
 
-function discountPriceCalculator(price){
+function discountPriceCalculator(price){       //Function to calculate discount price with price as a parameter
     
-    if(price>20){
-
-        let discountedPrice = price * (10/100);
+    if(price>20){                                  //Checking cutoff amount value for discount eligibility
+ 
+        let discountedPrice = price * (10/100);           //Calculating discount amount
         
-        let finalPrice = price - discountedPrice;
+        let finalPrice = price - discountedPrice;         //Final price after deducting discount amount
 
-        return finalPrice;
+        return finalPrice;                             //Returing the final price/amount
 
     }
-    else if(price<=20){
+    else{                               //if price if below the cutoff amount
 
-        return price;
+        return price;                  //returning the price amount
 
     }
 
 }
 
-console.log(discountPriceCalculator(25));
-console.log(discountPriceCalculator(20));
-console.log(discountPriceCalculator(15));
+//Function call for different test cases
+console.log(discountPriceCalculator(25));        //Test Case 1
+console.log(discountPriceCalculator(20));        //Test Case 2
+console.log(discountPriceCalculator(15));        //Test Case 3
+
