64b77b494860e86f609079a69a913917cd57deb6
Hitesh Kumar Siwan <hiteshk888@gmail.com>
2025-02-12 23:28:45 +0530

    added code for edge cases

--- DiscountPriceCalculator/Problem2_DiscountPriceCalculator.js
+++ DiscountPriceCalculator/Problem2_DiscountPriceCalculator.js
@@ -1,5 +1,7 @@
 
-function discountPriceCalculator(price){       //Function to calculate discount price with price as a parameter
+function discountPriceCalculator(price){ 
+    
+    //Function to calculate discount price with price as a parameter
     
     if(price>20){                                  //Checking cutoff amount value for discount eligibility
  
@@ -7,12 +9,20 @@ function discountPriceCalculator(price){       //Function to calculate discount 
         
         let finalPrice = price - discountedPrice;         //Final price after deducting discount amount
 
-        return finalPrice;                             //Returing the final price/amount
+        return "The final price of the item is: " + price;              //Returing the final price/amount
 
+    }
+    else if(price == 20){
+
+        return "No discount applied. \n"+"The final price of the item is: " + price;
+        
+    }
+    else if(price<0){
+        return "Invalid price, the price must be a non-negative number.";
     }
     else{                               //if price if below the cutoff amount
 
-        return price;                  //returning the price amount
+        return "No discount applied. \n"+"The final price of the item is: " + price;                  //returning the price amount
 
     }
 
@@ -22,4 +32,7 @@ function discountPriceCalculator(price){       //Function to calculate discount 
 console.log(discountPriceCalculator(25));        //Test Case 1
 console.log(discountPriceCalculator(20));        //Test Case 2
 console.log(discountPriceCalculator(15));        //Test Case 3
+
+console.log(discountPriceCalculator(20));        //Edge case 1
+console.log(discountPriceCalculator(-24));       //Edge case 2
 
