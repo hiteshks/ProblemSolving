e36454112f0c9cc5ceee92e8f52574eb9a2c98d6
Hitesh Kumar Siwan <hiteshk888@gmail.com>
2025-03-05 15:02:28 +0530

    Added the code

---
+++ AnagramDetector/anagramDetector.js
@@ -0,0 +1,29 @@
+function main(){
+
+    let s1 = "anagram";
+    let s2 = "nag a ram";
+
+    if(detectingAnagram(s1, s2)){
+        console.log("True")
+    }
+    else{
+        console.log("False")
+    }
+
+}
+
+
+
+
+function detectingAnagram(s1, s2){
+
+    s2 = s2.split(" ").join("");
+    s1 = s1.split("").sort((a,b)=> a.localeCompare(b)).join("");
+    s2 = s2.split("").sort((a,b)=> a.localeCompare(b)).join("");
+    console.log(s1, s2);
+
+    return s1 == s2 ? true : false;
+
+}
+
+main();
